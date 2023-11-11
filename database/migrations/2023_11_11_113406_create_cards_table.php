ac04819fc9c22075c175ab8764d2a91397a8a3a4
Lucie Gérard <tech.gerardl@gmail.com>
2023-11-11 13:08:18 +0100

    Create the tables with php artisan make:migration create_tableName_table then migrate them to the database with php artisan migrate

--- database/migrations/2023_11_11_113406_create_cards_table.php
+++ database/migrations/2023_11_11_113406_create_cards_table.php
@@ -11,13 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('cards', function (Blueprint $table) {
             $table->id();
-            $table->string('username');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('recto_name');
+            $table->string('verso_name');
+            $table->string('image')->nullable();
+            $table->foreignId('deck_id');
             $table->timestamps();
         });
     }
@@ -27,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('cards');
     }
 };
